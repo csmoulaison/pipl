8752983192eaafe34c9ec4a0c881a91015cca702
csmoulaison <csmoulaison@gmail.com>
2026-07-07 14:16:15 -0700

    Export obj and bmp from blend

--- code/model/main.c
+++ code/model/main.c
@@ -2,9 +2,18 @@
 #define CSM_SKIP_MATH
 #include "csm_core/core.h"
 
-char blend_cmd[] = "blender --background --python code/prebuild/blender_export.py -- assets/blend/%s.blend assets/obj/%s.obj assets/bmp/%s.bmp";
+#include "blender_export.c"
+
+char blend_cmd[] = "blender --background --python tmp.py -- %s.blend %s.obj %s.bmp";
 
 i32 main(i32 argc, char** argv) {
-    printf("Hello!\n");
+    FILE* py = fopen("tmp.py", "w");
+    fwrite(code_model_blender_export_py, code_model_blender_export_py_len, 1, py);
+    fclose(py);
+
+    char* asset_name = argv[1];
+    char cmd_buf[4096];
+    sprintf(cmd_buf, blend_cmd, asset_name, asset_name, asset_name);
+    system(cmd_buf);
     return 0;
 }
